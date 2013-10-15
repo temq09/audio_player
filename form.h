f6412fa82f99f11958d08f1f7397bbdcd31cd3e9
temq09 <artemjke@yandex.ru>
2013-10-15 18:03:04 +0400

    Implemented Switch track after end. Implementd show info about track

---
+++ form.h
@@ -0,0 +1,24 @@
+#ifndef FORM_H
+#define FORM_H
+
+#include <QWidget>
+#include <QString>
+
+namespace Ui {
+class Form;
+}
+
+class Form : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Form(QWidget *parent = 0);
+    Form(QWidget *parent, QString Title, QString Autor, QString Album, QString Genre, int Year, int NumTrack, int Duration, int Bitrate, int Size );
+    ~Form();
+
+private:
+    Ui::Form *ui;
+};
+
+#endif // FORM_H
